23acd63614ca2fc85a11012d8b756e4f008470cf
LinCie <aldiofernanda@protonmail.com>
2025-04-26 01:16:49 +0700

    feat: fix og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,8 +72,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://lincie.me/og-image.png",
-        width: 512,
-        height: 512,
+        width: 1200,
+        height: 630,
         alt: "LinCie",
       },
     ],
